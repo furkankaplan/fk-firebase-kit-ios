94b5bc74fdfb27e75104e04586e27c2ff27396a2
Furkan KAPLAN <furkankaplan@outlook.com>
2020-09-29 22:52:52 +0300

    Add example project

---
+++ fk-firebase-kit-ios/Example/fk-firebase-kit-ios-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fk-firebase-kit-ios-example
+//
+//  Created by Furkan Kaplan on 29.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
