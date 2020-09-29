91658bc66b7a7c823164bb2eb3079a9355b5bdec
Furkan KAPLAN <furkankaplan@outlook.com>
2020-09-29 23:04:34 +0300

    Add Cocoapods and Firebase dependency

---
+++ fk-firebase-kit-ios/Example/Pods/Target Support Files/Pods-fk-firebase-kit-ios-example/Pods-fk-firebase-kit-ios-example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_fk_firebase_kit_ios_example : NSObject
+@end
+@implementation PodsDummy_Pods_fk_firebase_kit_ios_example
+@end
---
+++ fk-firebase-kit-ios/Example/Pods/Target Support Files/Pods-fk-firebase-kit-ios-example/Pods-fk-firebase-kit-ios-example-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_fk_firebase_kit_ios_exampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_fk_firebase_kit_ios_exampleVersionString[];
+
