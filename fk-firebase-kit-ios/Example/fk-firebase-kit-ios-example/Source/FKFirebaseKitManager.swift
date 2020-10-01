6731b8a336a398ee2aca6bdff23ae5e035432f03
Furkan KAPLAN <furkankaplan@outlook.com>
2020-10-01 22:13:18 +0300

    Add request method for setting an object

--- fk-firebase-kit-ios/Example/fk-firebase-kit-ios-example/Source/FKFirebaseKitManager.swift
+++ fk-firebase-kit-ios/Example/fk-firebase-kit-ios-example/Source/FKFirebaseKitManager.swift
@@ -14,7 +14,23 @@ class FKFirebaseKitManager {
     public static let shared: FKFirebaseKitManager = FKFirebaseKitManager()
     
     /// Firebase database shared instance to use it interaction layers of all modules.
-    public static let database: DatabaseReference = Database.database().reference()
+    private let database: DatabaseReference = Database.database().reference()
     
+    func request(set data: Codable, endpoint: String..., onSuccess: @escaping(() -> Void), onError: @escaping((String) -> Void)) {
+        var innerDatabase: DatabaseReference = self.database
+        
+        for path in endpoint {
+            innerDatabase = innerDatabase.child(path)
+        }
+        
+        innerDatabase.setValue(data.toDictionary()) { (error: Error?, reference: DatabaseReference?) in
+            if let error = error {
+                onError(error.localizedDescription)
+                return
+            }
+            
+            onSuccess()
+        }
+    }
     
 }
